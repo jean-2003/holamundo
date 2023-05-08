d50f06b5bb7da1eb9003f1a26f7d399f881a97b3
JcHHP <jeancitouno@gmail.com>
2023-05-08 17:59:45 -0500

    Añadiendo formularios reactivos

--- src/app/cliente/cliente-add-edit/cliente-add-edit.module.ts
+++ src/app/cliente/cliente-add-edit/cliente-add-edit.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,7 +14,8 @@ import { ClienteAddEditPage } from './cliente-add-edit.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    ClienteAddEditPageRoutingModule
+    ClienteAddEditPageRoutingModule,
+    ReactiveFormsModule,
   ],
   declarations: [ClienteAddEditPage]
 })
--- src/app/cliente/cliente-add-edit/cliente-add-edit.page.ts
+++ src/app/cliente/cliente-add-edit/cliente-add-edit.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cliente-add-edit',
@@ -6,10 +7,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cliente-add-edit.page.scss'],
 })
 export class ClienteAddEditPage implements OnInit {
+  
+  clienteForm! : FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
 
-  constructor() { }
+  nombre!: string;
+
+  cliente = {
+    nombre : "",
+    email : "",
+    telefono : "",
+    ingreso : "",
+    nacimiento: "",
+  }
 
   ngOnInit() {
+    this.clienteForm = this.formBuilder.group({
+      nombre: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(50)
+      ])],
+
+      email: ['', Validators.compose([
+        Validators.required,
+        Validators.email,
+
+      ])],
+
+      telefono : ["", Validators.compose([
+        Validators.required,
+        Validators.minLength(9),
+        Validators.maxLength(9)
+      ])],
+
+      ingreso : ["", Validators.compose([
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(10)
+      ])],
+
+      nacimiento: ["", Validators.compose([
+        Validators.required,
+        Validators.maxLength(50)
+      ])]
+    });
+  }
+
+  submit(){
+    console.log(this.nombre);
+    this.nombre = "Jean";
   }
 
 }
